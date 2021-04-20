54f5bcc1336ee1ee9f423cfc4017238305fd59c7
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-20 21:44:10 +0600

    using Styled Components

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Style React and CSS Preprocessor SASS
 ### `Technology Used`
 - CSS Modules
+- Styled Components
 
 https://create-react-app.dev/
+https://styled-components.com/
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+const Container = styled.div`
+    margin: 10px;
+    color: purple;
+    border: 1px solid blue;
+`
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Homepage</h2>
+            <Wrapper>
+                <Title>I am using styled components</Title>
+            </Wrapper>
+
+            <Container>
+                <h3>Product Name</h3>
+                <p>Product Description</p>
+            </Container>
+            <Container>
+                <h3>Product Name</h3>
+                <p>Product Description</p>
+            </Container>
         </div>
     );
 };
