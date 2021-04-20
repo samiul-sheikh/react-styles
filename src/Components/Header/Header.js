099d207ce4a2249f2bc4545d94c59ba5f63fb019
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-20 18:06:10 +0600

    using react css modules

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h4 className={styles.title}>This is Header</h4>
+        </div>
+    );
+};
+
+export default Header;
